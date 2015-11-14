1163f75ba6f9cfe4a3c2304b713e5a6c10ad1589
Thin Nguyen Truong <truongthinnguyen@gmail.com>
2015-11-14 13:17:48 +0700

    Create roles for users

---
+++ database/seeds/RoleTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Role;
+use Illuminate\Database\Seeder;
+
+class RoleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->delete();
+        Role::create(array(
+            'name' => 'admin',
+            'description' => 'Administrator'
+        ));
+
+        Role::create(array(
+            'name' => 'viewer',
+            'description' => 'Viewer'
+        ));
+    }
+}
